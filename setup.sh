acc3cf041129c564dd628632a1b5408938e8e4d1
Roman Mikhailov <113858143+qfunky@users.noreply.github.com>
2026-04-07 21:03:15 +0300

    Simplify setup by removing swap file creation
    Removed swap file creation prompts and commands.

--- setup.sh
+++ setup.sh
@@ -4,24 +4,12 @@ REPO_RAW_URL="https://raw.githubusercontent.com/qfunky/schedule-parser/main"
 
 apt update && apt install python3-venv python3-pip git curl -y
 
-read -p "Создать Swap-файл 1ГБ? (y/n): " confirm_swap
-if [[ $confirm_swap == [yY] ]]; then
-    if [ ! -f /swapfile ]; then
-        fallocate -l 1G /swapfile || dd if=/dev/zero of=/swapfile bs=1M count=1024
-        chmod 600 /swapfile
-        mkswap /swapfile
-        swapon /swapfile || echo "Ошибка активации Swap"
-        grep -q '/swapfile' /etc/fstab || echo '/swapfile none swap sw 0 0' >> /etc/fstab
-    fi
-fi
-
 cd /root
 curl -L -O "$REPO_RAW_URL/server.py"
 
 python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install fastapi uvicorn icalendar pytz pyrinium
+/root/.venv/bin/pip install --upgrade pip
+/root/.venv/bin/pip install fastapi uvicorn icalendar pytz pyrinium
 
 cat << EOF > /etc/systemd/system/schedule-parser.service
 [Unit]
